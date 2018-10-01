636a43b373f21090c426d24aaad4c1af66d1f41c
ghuertaf <39933287+ghuertaf@users.noreply.github.com>
2018-09-30 22:34:33 -0600

    Lab 3 v. 1.1.0

--- Lab3/src/App.js
+++ Lab3/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navegacion';
 import {datos} from "./datos.json";
+import PersojaneForm from './components/PersonajeForm';
 
 
 class App extends Component {
@@ -11,8 +12,22 @@ class App extends Component {
     this.state = {
       datos
     }
-
+    this.handleAddPersonaje = this.handleAddPersonaje.bind(this);
   }
+ 
+    removePersonaje(index) {
+      this.setState({
+        datos: this.state.datos.filter((e, i) => {
+          return i !== index
+        })
+      });
+    }
+  
+    handleAddPersonaje(personaje) {
+      this.setState({
+        datos: [...this.state.datos, personaje]
+      })
+    }
 
   render() {
     const personajes = this.state.datos.map ((personaje, i) => {
@@ -27,22 +42,46 @@ class App extends Component {
             </div>            
             <div className = "card-body">
             <img src={personaje.icono}/>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removePersonaje.bind(this, i)}>
+                Delete
+              </button>
+            </div>
             </div>
           </div>
         </div>
       )
-    })
+    });
 
     return (
       <div className="App">
-        
-        <Navigation titulo="Personajes"/>
+
+      <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Personajes
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.datos.length}
+            </span>
+          </a>
+        </nav>
+
         <div className="container"> 
           <div className="row mt-4">
-            {personajes}
+
+            <div className="col-md-4 text-center">
+              <img src={logo} className="App-logo" alt="logo" />
+            <PersojaneForm onAddPersonaje={this.handleAddPersonaje}></PersojaneForm>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+              {personajes}
+              </div>
+            </div>
           </div>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
---
+++ Lab3/src/components/PersonajeForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+class PersojaneForm extends Component {
+  constructor () {
+    super();
+    this.state = {
+      nombre: '',
+      clase: '',
+      icono: '',
+      dificutad: 'low'
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onAddPersonaje(this.state);
+    this.setState({
+      nombre: '',
+      clase: '',
+      icono: '',
+      dificutad: 'low'
+    });
+  }
+
+  handleInputChange(e) {
+    const {value, name} = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <form onSubmit={this.handleSubmit} className="card-body">
+          <div className="form-group">
+            <input
+              type="text"
+              name="nombre"
+              className="form-control"
+              value={this.state.nombre}
+              onChange={this.handleInputChange}
+              placeholder="nombre"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="clase"
+              className="form-control"
+              value={this.state.clase}
+              onChange={this.handleInputChange}
+              placeholder="clase"
+              />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              name="icono"
+              className="form-control"
+              value={this.state.icono}
+              onChange={this.handleInputChange}
+              placeholder="icono"
+              />
+          </div>
+          <div className="form-group">
+            <select
+                name="dificutad"
+                className="form-control"
+                value={this.state.dificutad}
+                onChange={this.handleInputChange}
+              >
+              <option>low</option>
+              <option>medium</option>
+              <option>high</option>
+            </select>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default PersojaneForm;
